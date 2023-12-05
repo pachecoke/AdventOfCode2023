c0ec09e996e6d114938c5b8621037d0149bb09d8
Kevin Pacheco <pacheco.ke@outlook.com>
2023-12-05 18:53:39 -0500

    Solve day 1 part 2

--- day1/day1_test.py
+++ day1/day1_test.py
@@ -3,15 +3,37 @@ from day1 import main
 
 
 class Day1Tests(unittest.TestCase):
-    def test_get_first_digit_in_calibration(self):
+    def test_get_first_digit_in_calibration1(self):
         amended_calibration_value = "1abc2"
-        calibration_value = main.get_first_digit(amended_calibration_value)
+        calibration_value, index = main.get_first_digit(amended_calibration_value)
         self.assertEqual(calibration_value, 1)
+        self.assertEqual(index, 0)
+
+    def test_get_first_calibration_value_when_digit(self):
+        amended_calibration_value = "z3oneight234"
+        calibration_value = main.get_first_calibration_value(amended_calibration_value)
+        self.assertEqual(calibration_value, 3)
+
+    def test_get_first_calibration_value_when_word(self):
+        amended_calibration_value = "zoneight234"
+        calibration_value = main.get_first_calibration_value(amended_calibration_value)
+        self.assertEqual(calibration_value, 1)
+
+    def test_get_last_calibration_value_when_digit(self):
+        amended_calibration_value = "zoneight234"
+        calibration_value = main.get_first_calibration_value(amended_calibration_value)
+        self.assertEqual(calibration_value, 1)
+
+    def test_get_last_calibration_value_when_word(self):
+        amended_calibration_value = "zoneight234nine"
+        calibration_value = main.get_last_calibration_value(amended_calibration_value)
+        self.assertEqual(calibration_value, 9)
 
     def test_get_last_digit_in_calibration(self):
         amended_calibration_value = "1abc2"
-        calibration_value = main.get_last_digit(amended_calibration_value)
+        calibration_value, index = main.get_last_digit(amended_calibration_value)
         self.assertEqual(calibration_value, 2)
+        self.assertEqual(index, 4)
 
     def test_recover_calibration_value(self):
         amended_calibration_value = "1abc2"
@@ -27,3 +49,20 @@ class Day1Tests(unittest.TestCase):
         calibration_values = [12, 34, 56]
         sum = main.sum_calibration_values(calibration_values)
         self.assertEqual(sum, 102)
+
+    def test_get_first_number_word(self):
+        input = "zoneight234"
+        first_number_word, index = main.get_first_number_word(input)
+        self.assertEqual(first_number_word, "one")
+        self.assertEqual(index, 1)
+
+    def test_get_last_number_word(self):
+        input = "zoneight234two"
+        last_number_word, index = main.get_last_number_word(input)
+        self.assertEqual(last_number_word, "two")
+        self.assertEqual(index, 11)
+
+    def test_get_index_of_word(self):
+        input = "ztwoight234two"
+        indices = main.get_indices_of_word("two", input)
+        self.assertEqual(indices, [1, 11])
--- day1/main.py
+++ day1/main.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 
 def read_input(input_file_name):
@@ -7,31 +8,49 @@ def read_input(input_file_name):
     return text
 
 
+def get_first_calibration_value(amended_calibration_value):
+    digit_value, digit_index = get_first_digit(amended_calibration_value)
+    word_value, word_index = get_first_number_word(amended_calibration_value)
+    if digit_index != -1 and digit_index < word_index:
+        return digit_value
+    return word_to_digit[word_value]
+
+
+def get_last_calibration_value(amended_calibration_value):
+    digit_value, digit_index = get_last_digit(amended_calibration_value)
+    word_value, word_index = get_last_number_word(amended_calibration_value)
+    if digit_index > word_index:
+        return digit_value
+    return word_to_digit[word_value]
+
+
 def get_first_digit(amended_calibration_value):
     for character in amended_calibration_value:
         if character.isdigit():
             first = int(character)
-            return first
-    raise Exception(f"No digits found in the input: {amended_calibration_value}")
+            return first, amended_calibration_value.find(str(first))
+    return "", -1
 
 
 def get_last_digit(amended_calibration_value):
     for character in reversed(amended_calibration_value):
         if character.isdigit():
             last = int(character)
-            return last
-    raise Exception(f"No digits found in the input: {amended_calibration_value}")
+            return last, amended_calibration_value.rfind(str(last))
+    return "", -1
 
 
 def recover_calibration_value(amended_calibration_value):
-    first = get_first_digit(amended_calibration_value)
-    last = get_last_digit(amended_calibration_value)
+    first = get_first_calibration_value(amended_calibration_value)
+    last = get_last_calibration_value(amended_calibration_value)
     return int(f"{first}{last}")
 
 
 def recover_all_calibration_values(amended_calibration_values):
     calibration_values = []
+    i = 0
     for amended_calibration_value in amended_calibration_values.split("\n"):
+        i = i + 1
         if amended_calibration_value == "":
             continue
         calibration_value = recover_calibration_value(amended_calibration_value)
@@ -43,7 +62,57 @@ def sum_calibration_values(calibration_values):
     return sum(calibration_values)
 
 
-def solve_puzzle_part_1(input):
+word_to_digit = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+
+def get_indices_of_word(word, input):
+    return [m.start() for m in re.finditer(word, input)]
+
+
+def get_indices_of_words(input):
+    indices = {}
+    for word in word_to_digit:
+        word_indices = get_indices_of_word(word, input)
+        if word_indices != []:
+            indices[word] = word_indices
+    return indices
+
+
+def get_first_number_word(input):
+    indices = get_indices_of_words(input)
+    smallest_index = sys.maxsize
+    smallest_word = None
+    for word in indices:
+        word_indices = indices[word]
+        if min(word_indices) < smallest_index:
+            smallest_index = min(word_indices)
+            smallest_word = word
+    return smallest_word, smallest_index
+
+
+def get_last_number_word(input):
+    indices = get_indices_of_words(input)
+    largest_index = -1
+    largest_word = None
+    for word in indices:
+        word_indices = indices[word]
+        if max(word_indices) > largest_index:
+            largest_index = max(word_indices)
+            largest_word = word
+    return largest_word, largest_index
+
+
+def solve_puzzle(input):
     calibration_values = recover_all_calibration_values(input)
     sum = sum_calibration_values(calibration_values)
     print(sum)
@@ -52,4 +121,4 @@ def solve_puzzle_part_1(input):
 if __name__ == "__main__":
     file_input = sys.argv[1]
     input = read_input(file_input)
-    solve_puzzle_part_1(input)
+    solve_puzzle(input)
